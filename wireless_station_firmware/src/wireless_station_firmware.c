9e145d17ffeb718ea3e374b4713c3ea40e4da08f
Roi Lopez Barata <137203489+RoiCorporation@users.noreply.github.com>
2026-03-17 09:58:53 +0100

    Working version with the wireless station firmware slightly refactored.

--- wireless_station_firmware/src/wireless_station_firmware.c
+++ wireless_station_firmware/src/wireless_station_firmware.c
@@ -9,6 +9,7 @@
 #include "alerts.h"
 #include "errors.h"
 
+void handle_nrf(nrf_client_t* nrf);
 
 int main() {
     
@@ -29,7 +30,6 @@ int main() {
     sleep_ms(180); // Wait for the first measurement (~120-180ms for high res).
 
 
-    pin_manager_t my_pins = { .sck = SCK_PIN, .copi = COPI_PIN, .cipo = CIPO_PIN, .csn = CS_PIN, .ce = CE_PIN };
 
 
 
@@ -73,136 +73,30 @@ int main() {
     //     sleep_ms(1000); //sleep_ms(MINUTE_IN_MILLISECONDS)
     // }
 
-      /**
-     * nrf_manager_t can be passed to the nrf_client_t
-     * initialise function, to specify the NRF24L01 
-     * configuration. If NULL is passed to the initialise 
-     * function, then the default configuration will be used.
-     */
-    nrf_manager_t my_config = {
-        // RF Channel 
-        .channel = 120,
 
-        // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
-        .address_width = AW_5_BYTES,
-
-        // dynamic payloads: DYNPD_ENABLE, DYNPD_DISABLE
-        .dyn_payloads = DYNPD_ENABLE,
-
-        // data rate: RF_DR_250KBPS, RF_DR_1MBPS, RF_DR_2MBPS
-        .data_rate = RF_DR_1MBPS,
-
-        // RF_PWR_NEG_18DBM, RF_PWR_NEG_12DBM, RF_PWR_NEG_6DBM, RF_PWR_0DBM
-        .power = RF_PWR_NEG_12DBM,
-
-        // retransmission count: ARC_NONE...ARC_15RT
-        .retr_count = ARC_10RT,
-
-        // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
-        .retr_delay = ARD_500US 
-    };
-
-    // SPI baudrate
-    uint32_t my_baudrate = 5000000;
-
-    nrf_client_t my_nrf;
-
-    // initialise my_nrf
-    nrf_driver_create_client(&my_nrf);
-
-    // configure GPIO pins and SPI
-    my_nrf.configure(&my_pins, my_baudrate);
-
-    // not using default configuration (my_nrf.initialise(NULL)) 
-    my_nrf.initialise(&my_config);
-
-    // set to Standby-I Mode
-    my_nrf.standby_mode();
-
-    // payload sent to receiver data pipe 0
-    uint8_t payload_zero = 123;
+    nrf_client_t nrf24_module;
+    handle_nrf(&nrf24_module);
 
     // payload sent to receiver data pipe 1
-    uint8_t payload_one[5] = "Hello";
-
-    typedef struct payload_two_s { uint8_t one; uint8_t two; } payload_two_t;
-
-    // payload sent to receiver data pipe 2
-    payload_two_t payload_two = { .one = 123, .two = 213 };
+    uint8_t payload_one[6] = "Hello\0";
 
     // result of packet transmission
     fn_status_t success = 0;
 
-    uint64_t time_sent = 0; // time packet was sent
-    uint64_t time_reply = 0; // response time after packet sent
 
     while (1) {
 
-        // send to receiver's DATA_PIPE_0 address
-        my_nrf.tx_destination((uint8_t[]){0x37,0x37,0x37,0x37,0x37});
-
-        // time packet was sent
-        time_sent = to_us_since_boot(get_absolute_time()); // time sent
-
-        // send packet to receiver's DATA_PIPE_0 address
-        success = my_nrf.send_packet(&payload_zero, sizeof(payload_zero));
-
-        // time auto-acknowledge was received
-        time_reply = to_us_since_boot(get_absolute_time()); // response time
-
-        if (success)
-        {
-        printf("\nPacket sent:- Response: %lluμS | Payload: %d\n", time_reply - time_sent, payload_zero);
-
-        } else {
-
-        printf("\nPacket not sent:- Receiver not available.\n");
-        }
-
-        sleep_ms(3000);
-
         // send to receiver's DATA_PIPE_1 address
-        my_nrf.tx_destination((uint8_t[]){0xC7,0xC7,0xC7,0xC7,0xC7});
-
-        // time packet was sent
-        time_sent = to_us_since_boot(get_absolute_time()); // time sent
+        nrf24_module.tx_destination((uint8_t[]){0xC7,0xC7,0xC7,0xC7,0xC7});
 
         // send packet to receiver's DATA_PIPE_1 address
-        success = my_nrf.send_packet(payload_one, sizeof(payload_one));
-        
-        // time auto-acknowledge was received
-        time_reply = to_us_since_boot(get_absolute_time()); // response time
 
-        if (success)
-        {
-        printf("\nPacket sent:- Response: %lluμS | Payload: %s\n", time_reply - time_sent, payload_one);
-
-        } else {
-
-        printf("\nPacket not sent:- Receiver not available.\n");
-        }
-
-        sleep_ms(500);
-
-        // send to receiver's DATA_PIPE_2 address
-        my_nrf.tx_destination((uint8_t[]){0xC8,0xC7,0xC7,0xC7,0xC7});
-
-        // time packet was sent
-        time_sent = to_us_since_boot(get_absolute_time()); // time sent
-
-        // send packet to receiver's DATA_PIPE_2 address
-        success = my_nrf.send_packet(&payload_two, sizeof(payload_two));
-        
-        // time auto-acknowledge was received
-        time_reply = to_us_since_boot(get_absolute_time()); // response time
-
-        if (success)
-        {
-        printf("\nPacket sent:- Response: %lluμS | Payload: %d & %d\n",time_reply - time_sent, payload_two.one, payload_two.two);
-
-        } else {
-
-        printf("\nPacket not sent:- Receiver not available.\n");
+        success = nrf24_module.send_packet(payload_one, sizeof(payload_one));
+        if (success) {
+            printf("Packet sent: %s\n", payload_one);
+        } 
+        else {
+            printf("Packet not sent:- Receiver not available.\n");
         }
 
         sleep_ms(500);
@@ -210,4 +104,22 @@ int main() {
     
 
 
+}
+
+void handle_nrf(nrf_client_t* nrf24_module) {
+
+    pin_manager_t nrf24_pins = { .sck = SCK_PIN, .copi = COPI_PIN, .cipo = CIPO_PIN, .csn = CS_PIN, .ce = CE_PIN };
+
+    // initialise my_nrf
+    nrf_driver_create_client(nrf24_module);
+
+    // configure GPIO pins and SPI
+    nrf24_module->configure(&nrf24_pins, SPI_BAUDRATE);
+    nrf24_module->dyn_payloads_enable();
+
+    // not using default configuration (my_nrf.initialise(NULL)) 
+    nrf24_module->initialise(NULL);
+
+    // set to Standby-I Mode
+    nrf24_module->standby_mode();
 }
